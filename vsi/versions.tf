8714ca7ffcdd3d89cef4aacfbcffb9fd3e6c820b
Hassian Niazi <hassan.niazi@uk.ibm.com>
2024-06-13 18:04:51 +0100

    initial commit

---
+++ vsi/versions.tf
@@ -0,0 +1,21 @@
+##############################################################################
+# Terraform Providers
+##############################################################################
+
+terraform {
+  required_version = ">= 1.3"
+  # renovate is set up to keep provider version at the latest for all DA solutions
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "1.66.0"
+    }
+    # tflint-ignore: terraform_unused_required_providers
+    external = {
+      source  = "hashicorp/external"
+      version = "2.3.3"
+    }
+  }
+}
+
+##############################################################################
